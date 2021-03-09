3e3c7e246b173e8a2f03d1122f2acf1ece2634ae
Michele Boggia <michele.boggia@helsinki.fi>
2021-03-09 11:03:04 +0200

    mem reduced in job script

--- job_scripts/job_d2t_iterative_editing_cpu.sh
+++ job_scripts/job_d2t_iterative_editing_cpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=70:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=16000
+#SBATCH --mem-per-cpu=1600
 #SBATCH --mail-type=BEGIN,FAIL,END
 
 module load python-data/3.7.6-1
